fe537dc0b0ed4e4ff8ba6702c1e32fd015664a0d
Sergii Gama <balls@sergiigama.com>
2018-06-19 03:44:27 +0300

    Modify module `init` method to use application-level layouts path

--- Module.php
+++ Module.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace webkadabra\yii\modules\cms;
+
+use \yii;
 /**
  * Class Module
  * @author Sergii Gamaiunov <devkadabra@gmail.com>
@@ -9,4 +11,10 @@ namespace webkadabra\yii\modules\cms;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'webkadabra\yii\modules\cms\controllers';
+
+    public function init()
+    {
+        parent::init();
+        $this->layoutPath = Yii::getAlias('@app/views/layouts'); // use application-level layouts, not module level;
+    }
 }
