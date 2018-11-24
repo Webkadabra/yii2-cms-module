26a483769205999008641677c224b0b27d987a31
Sergii Gama <balls@sergiigama.com>
2018-11-24 04:06:35 +0200

    Add `CmsControllerTrait`

---
+++ components/CmsControllerTrait.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Alphatech, <http://www.alphatech.com.ua>
+ *
+ * Copyright (C) 2018-present Sergii Gamaiunov <devkadabra@gmail.com>
+ * All rights reserved.
+ */
+
+namespace deadpool\components;
+
+use webkadabra\yii\modules\cms\models\CmsContentBlock;
+use webkadabra\yii\modules\cms\models\CmsRoute;
+use yii;
+
+trait CmsControllerTrait
+{
+    protected function parseBlock($in) {
+        return $in;
+    }
+
+    public function printBlock($name, $capture = false) {
+        $this->preparePageBlocks();
+        $output = '';
+        if (!empty($this->contentBlocks[$name])) {
+            foreach ($this->contentBlocks[$name] as $blockKey => $blockContent) {
+                if ($capture) {
+                    $output .= $this->parseBlock($blockContent);
+                } else {
+                    echo $this->parseBlock($blockContent);
+                }
+            }
+        }
+        if (!empty($this->cmsCssBlocks[$name])) {
+            foreach ($this->cmsCssBlocks[$name] as $blockKey => $blockConfig) {
+                if (isset($blockConfig['type']) && isset($blockConfig['value'])) {
+                    if ($blockConfig['type'] == CmsContentBlock::TYPE_CSS_INLINE) {
+                        $this->view->registerCss($blockConfig['value']);
+                    } else if ($blockConfig['type'] == CmsContentBlock::TYPE_CSS_LINK) {
+                        $this->view->registerCssFile($blockConfig['value'], [
+                            'depends' => [
+                                'frontend\assets\AppAsset'
+                            ]]);
+                    }
+                }
+            }
+        }
+        if (!empty($this->cmsJsBlocks[$name])) {
+            foreach ($this->cmsJsBlocks[$name] as $blockKey => $blockConfig) {
+                if (isset($blockConfig['type']) && isset($blockConfig['value'])) {
+                    if ($blockConfig['type'] == CmsContentBlock::TYPE_JAVASCRIPT_FILE) {
+                        $this->view->registerJsFile($blockConfig['value'], ['position' => View::POS_END]);
+                    } else if ($blockConfig['type'] == CmsContentBlock::TYPE_JAVASCRIPT_INLINE) {
+                        $this->view->registerJs($blockConfig['value']);
+                    }
+                }
+            }
+        }
+        if ($capture) {
+            return $output;
+        }
+
+        return null;
+    }
+
+    public function hasContentBlock($name) {
+        $this->preparePageBlocks();
+        return !empty($this->contentBlocks[$name]);
+    }
+
+    public $contentBlocks;
+    protected $cmsCssBlocks = array();
+    protected $cmsJsBlocks = array();
+    protected $pagePrepared = false;
+
+    /**
+     * @todo refactor as trait
+     */
+    protected function preparePageBlocks() {
+        if ($node = $this->getCmsNode()) {
+            if ($this->pagePrepared === false) {
+                foreach ($node->localizedContentBlocksArray as $value) {
+                    $blockName = (isset($value['contentBlockName']) ? $value['contentBlockName'] : $i++);
+                    if ($value['contentType'] == CmsContentBlock::TYPE_HTML)
+                        $this->contentBlocks[$blockName][] = $value['content'];
+                    else if (in_array($value['contentType'], [CmsContentBlock::TYPE_CSS_LINK, CmsContentBlock::TYPE_CSS_INLINE]))
+                        $this->cmsCssBlocks[$blockName][] = [
+                            'type' => $value['contentType'],
+                            'value' => $value['content'],
+                        ];
+                    else if (in_array($value['contentType'], [CmsContentBlock::TYPE_JAVASCRIPT_FILE, CmsContentBlock::TYPE_JAVASCRIPT_INLINE]))
+                        $this->cmsJsBlocks[$blockName][] = [
+                            'type' => $value['contentType'],
+                            'value' => $value['content'],
+                        ];
+                }
+                $this->pagePrepared = true;
+            }
+        }
+
+    }
+
+    public function hasCmsNode() {
+        if (isset(Yii::$app->params['view_preview_node']) && Yii::$app->params['view_preview_node']) {
+            return Yii::$app->params['view_preview_node'];
+        }
+        return isset(Yii::$app->params['view_node']) && Yii::$app->params['view_node'];
+    }
+
+    /**
+     * @return CmsRoute
+     */
+    public function getCmsNode() {
+        if (isset(Yii::$app->params['view_preview_node']) && Yii::$app->params['view_preview_node']) {
+            return Yii::$app->params['view_preview_node'];
+        }
+        return isset(Yii::$app->params['view_node']) && Yii::$app->params['view_node'] ? Yii::$app->params['view_node'] : null;
+    }
+
+    public function getViewName($base) {
+        return isset(Yii::$app->params['view_template']) ? Yii::$app->params['view_template'] : $base;
+    }
+}
