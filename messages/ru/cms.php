5ba85e4c77fb4c2f17d4b0caa046c89df9ec6cc4
Sergii Gama <balls@sergiigama.com>
2018-12-10 14:57:31 +0200

    Add i18n translations

--- messages/ru/cms.php
+++ messages/ru/cms.php
@@ -1,9 +1,24 @@
 <?php
 return [
+    'Save' => 'Сохранить',
+    'Changes saved' => 'Настройки сохранены',
+
     'Create New Version' => 'Создать новую версию',
     'Add another site' => 'Добавить сайт',
     'Domain' => 'Домен',
     'Site Name' => 'Название',
     'Code' => 'Код',
     'Active' => 'Включён',
+
+    'Websites' => 'Сайты',
+    'Content' => 'Содержание',
+
+    'Internal use only' => 'Отображается только в админке',
+    'App ID as it appears in `config.php` (for multiple websites only)' => 'ID сайта в `config.php` (только если вы используете несколько сайтов)',
+    'Website dmain, e.g. `example.com`' => 'Домен сайта, например `example.com`',
+    'Site URL' => 'Адрес сайта',
+    'E.g. `https://example.com`' => 'Например `https://example.com`',
+    'Optional custom URL manager component name, available in `Yii::$app`. By default, component `urlManager` will be used.' => 'Название компонента из `Yii::$app` для управления URL. По умолчанию, будет использован стандартный `urlManager`.',
+
+    'This field is mandatory.' => 'Это обязательный пункт.',
 ];
